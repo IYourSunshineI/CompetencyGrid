e4fcbbc47d3a6913447dfd4133253f79fd7e4457
Sebastian Klaus <e12120487@student.tuwien.ac.at>
2023-01-21 16:31:08 +0100

    Template Header Creation

--- CreateTemplateForm.Designer.cs
+++ CreateTemplateForm.Designer.cs
@@ -23,60 +23,177 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.templateName = new System.Windows.Forms.Label();
-            this.tempTextBox = new System.Windows.Forms.RichTextBox();
-            this.btn_safe = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelKlasse = new System.Windows.Forms.Label();
+            this.labelSchuljahr = new System.Windows.Forms.Label();
+            this.labelSemester = new System.Windows.Forms.Label();
+            this.panel_create = new System.Windows.Forms.Panel();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.tb_header = new CueTextBox();
+            this.tb_semester = new CueTextBox();
+            this.tb_schuljahr = new CueTextBox();
+            this.tb_klasse = new CueTextBox();
+            this.tb_fileName = new CueTextBox();
+            this.panel_create.SuspendLayout();
             this.SuspendLayout();
             // 
-            // templateName
+            // labelName
             // 
-            this.templateName.AutoSize = true;
-            this.templateName.Location = new System.Drawing.Point(12, 9);
-            this.templateName.Name = "templateName";
-            this.templateName.Size = new System.Drawing.Size(60, 23);
-            this.templateName.TabIndex = 0;
-            this.templateName.Text = "Name";
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 15);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 23);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
             // 
-            // tempTextBox
+            // labelKlasse
             // 
-            this.tempTextBox.Location = new System.Drawing.Point(12, 35);
-            this.tempTextBox.Name = "tempTextBox";
-            this.tempTextBox.Size = new System.Drawing.Size(477, 229);
-            this.tempTextBox.TabIndex = 2;
-            this.tempTextBox.Text = "";
+            this.labelKlasse.AutoSize = true;
+            this.labelKlasse.Location = new System.Drawing.Point(12, 51);
+            this.labelKlasse.Name = "labelKlasse";
+            this.labelKlasse.Size = new System.Drawing.Size(66, 23);
+            this.labelKlasse.TabIndex = 2;
+            this.labelKlasse.Text = "Klasse";
             // 
-            // btn_safe
+            // labelSchuljahr
             // 
-            this.btn_safe.Font = new System.Drawing.Font("Roboto-Light", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_safe.Location = new System.Drawing.Point(411, 270);
-            this.btn_safe.Name = "btn_safe";
-            this.btn_safe.Size = new System.Drawing.Size(78, 23);
-            this.btn_safe.TabIndex = 3;
-            this.btn_safe.Text = "Speichern";
-            this.btn_safe.UseVisualStyleBackColor = true;
-            this.btn_safe.Click += new System.EventHandler(this.btn_safe_Click);
+            this.labelSchuljahr.AutoSize = true;
+            this.labelSchuljahr.Location = new System.Drawing.Point(246, 51);
+            this.labelSchuljahr.Name = "labelSchuljahr";
+            this.labelSchuljahr.Size = new System.Drawing.Size(85, 23);
+            this.labelSchuljahr.TabIndex = 5;
+            this.labelSchuljahr.Text = "Schuljahr";
+            // 
+            // labelSemester
+            // 
+            this.labelSemester.AutoSize = true;
+            this.labelSemester.Location = new System.Drawing.Point(12, 87);
+            this.labelSemester.Name = "labelSemester";
+            this.labelSemester.Size = new System.Drawing.Size(90, 23);
+            this.labelSemester.TabIndex = 8;
+            this.labelSemester.Text = "Semester";
+            // 
+            // panel_create
+            // 
+            this.panel_create.Controls.Add(this.tb_header);
+            this.panel_create.Controls.Add(this.labelHeader);
+            this.panel_create.Controls.Add(this.btn_save);
+            this.panel_create.Controls.Add(this.tb_semester);
+            this.panel_create.Controls.Add(this.tb_schuljahr);
+            this.panel_create.Controls.Add(this.tb_klasse);
+            this.panel_create.Controls.Add(this.tb_fileName);
+            this.panel_create.Controls.Add(this.labelName);
+            this.panel_create.Controls.Add(this.labelKlasse);
+            this.panel_create.Controls.Add(this.labelSemester);
+            this.panel_create.Controls.Add(this.labelSchuljahr);
+            this.panel_create.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_create.Location = new System.Drawing.Point(0, 0);
+            this.panel_create.Name = "panel_create";
+            this.panel_create.Size = new System.Drawing.Size(584, 361);
+            this.panel_create.TabIndex = 11;
+            // 
+            // btn_save
+            // 
+            this.btn_save.AutoSize = true;
+            this.btn_save.Font = new System.Drawing.Font("Roboto-Light", 10F);
+            this.btn_save.Location = new System.Drawing.Point(493, 322);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(79, 27);
+            this.btn_save.TabIndex = 16;
+            this.btn_save.Text = "Speichern";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Location = new System.Drawing.Point(12, 123);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(70, 23);
+            this.labelHeader.TabIndex = 16;
+            this.labelHeader.Text = "Header";
+            // 
+            // tb_header
+            // 
+            this.tb_header.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_header.Cue = "Schule, SKZ, Adr.";
+            this.tb_header.Location = new System.Drawing.Point(119, 120);
+            this.tb_header.Name = "tb_header";
+            this.tb_header.Size = new System.Drawing.Size(453, 30);
+            this.tb_header.TabIndex = 15;
+            this.tb_header.Text = "Volksschule Würflach, SKZ: 318.501, 2732 Würflach, Neunkirchner Str. 76";
+            this.tb_header.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_semester
+            // 
+            this.tb_semester.Cue = "1 od. 2";
+            this.tb_semester.Location = new System.Drawing.Point(119, 84);
+            this.tb_semester.Name = "tb_semester";
+            this.tb_semester.Size = new System.Drawing.Size(121, 30);
+            this.tb_semester.TabIndex = 14;
+            this.tb_semester.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_schuljahr
+            // 
+            this.tb_schuljahr.Cue = "z.b. 2022/23";
+            this.tb_schuljahr.Location = new System.Drawing.Point(337, 51);
+            this.tb_schuljahr.Name = "tb_schuljahr";
+            this.tb_schuljahr.Size = new System.Drawing.Size(121, 30);
+            this.tb_schuljahr.TabIndex = 13;
+            this.tb_schuljahr.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_klasse
+            // 
+            this.tb_klasse.Cue = "z.b. 1";
+            this.tb_klasse.Location = new System.Drawing.Point(119, 48);
+            this.tb_klasse.Name = "tb_klasse";
+            this.tb_klasse.Size = new System.Drawing.Size(121, 30);
+            this.tb_klasse.TabIndex = 12;
+            this.tb_klasse.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_fileName
+            // 
+            this.tb_fileName.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_fileName.Cue = "z.b. Vorlage 1. Klasse";
+            this.tb_fileName.Location = new System.Drawing.Point(119, 12);
+            this.tb_fileName.Name = "tb_fileName";
+            this.tb_fileName.Size = new System.Drawing.Size(453, 30);
+            this.tb_fileName.TabIndex = 11;
+            this.tb_fileName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // CreateTemplateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 305);
-            this.Controls.Add(this.btn_safe);
-            this.Controls.Add(this.tempTextBox);
-            this.Controls.Add(this.templateName);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.panel_create);
             this.Font = new System.Drawing.Font("Roboto-Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "CreateTemplateForm";
             this.Text = "Vorlage erstellen";
+            this.panel_create.ResumeLayout(false);
+            this.panel_create.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label templateName;
-        private System.Windows.Forms.RichTextBox tempTextBox;
-        private System.Windows.Forms.Button btn_safe;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelKlasse;
+        private System.Windows.Forms.Label labelSchuljahr;
+        private System.Windows.Forms.Label labelSemester;
+        private System.Windows.Forms.Panel panel_create;
+        private CueTextBox tb_semester;
+        private CueTextBox tb_schuljahr;
+        private CueTextBox tb_klasse;
+        private CueTextBox tb_fileName;
+        private System.Windows.Forms.Button btn_save;
+        private CueTextBox tb_header;
+        private System.Windows.Forms.Label labelHeader;
     }
 }
--- CreateTemplateForm.cs
+++ CreateTemplateForm.cs
@@ -1,36 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Font = iTextSharp.text.Font;
 
 namespace CompetencyGrid {
     public partial class CreateTemplateForm : Form {
-        string filePath;
+        string path = null;
+        Font calibriDef = FontFactory.GetFont("Calibri Regular", 10);
+        Font calibriBold = FontFactory.GetFont("Calibri Regular", BaseFont.CP1252, BaseFont.EMBEDDED, 12, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK);
+        Font calibriHeader = FontFactory.GetFont("Calibri Regular", 11);
+        Document doc;
 
         public CreateTemplateForm() {
             InitializeComponent();
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = Application.StartupPath;
-            saveFileDialog.RestoreDirectory = true;
-            saveFileDialog.DefaultExt = "txt";
-            if (saveFileDialog.ShowDialog() == DialogResult.OK) {
-                filePath = saveFileDialog.FileName;
-                string temp = Path.GetFileNameWithoutExtension(filePath);
-                templateName.Text = "Vorlage: " + temp;
-            }
         }
 
-        private void btn_safe_Click(object sender, EventArgs e) {
-            System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath);
-            writer.Write(tempTextBox.Text);
-            writer.Close();
-            writer.Dispose();
+        private void btn_save_Click(object sender, EventArgs e) {
+            //save the file
+
+            //check inputs
+            bool _break = false;
+            var alltbs = panel_create.Controls.OfType<CueTextBox>();
+            foreach (CueTextBox tb in alltbs) {
+                if (tb.Text == null || tb.Text == string.Empty) {
+                    tb.BackColor = Color.FromArgb(255, 128, 128);
+                    _break = true;
+                    continue;
+                }
+                tb.BackColor = Color.White;
+            }
+            if (_break)
+                return;
+
+            if (path == null) {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK) {
+                    path = fbd.SelectedPath;
+                } else {
+                    Console.Error.WriteLine("Error occourd on path selection");
+                    return;
+                }
+            }
+
+            //create Document and write header
+            doc = new Document();
+            try {
+                PdfWriter.GetInstance(doc, new FileStream(path + "/" + tb_fileName.Text + ".pdf", FileMode.Create));
+                doc.Open();
+                Paragraph paragraph = new Paragraph(tb_header.Text + ", Schuljahr " + tb_schuljahr.Text + "\n\n", calibriHeader);
+                doc.Add(paragraph);
+                paragraph = new Paragraph("Schriftliche Erläuterung/Kompetenzkatalog für Vorname Nachname\nSchüler/in der " + tb_klasse.Text +
+                    ". Klasse im " + tb_semester.Text + ". Semester", calibriBold);
+                doc.Add(paragraph);
+            } catch (DocumentException dex) {
+                throw (dex);
+            } catch (IOException ioex) {
+                throw (ioex);
+            } finally {
+                Console.WriteLine("done");
+                doc.Close(); //!!
+            }
         }
     }
 }
